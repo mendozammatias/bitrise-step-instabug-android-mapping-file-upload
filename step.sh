d7d78718080ea367c6364a06a92a93b32af5480a
mendozammatias- <matimendoza@frba.utn.edu.ar>
2024-02-12 15:58:15 -0300

    fix(logs): replaced files for file since its only one file

--- step.sh
+++ step.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-echo "Instabug mapping files uploader"
+echo "Instabug mapping file uploader"
 
 echo "Build gradle path $build_gradle_path"
 
@@ -26,21 +26,24 @@ envman add --key VERSION_NAME --value $VERSION_NAME
 VERSION='{"code":"'"$VERSION_CODE"'","name":"'"$VERSION_NAME"'"}'
 
 if [ ! -f $BITRISE_MAPPING_PATH ]; then
-    echo "Mapping files not found. Did you run this step before the build step?"
+    echo "Mapping file not found. Did you run this step before the build step?"
     exit 1
 fi
 
-echo "Mapping files found! Uploading..."
+echo "Mapping file found! Uploading..."
 
 ENDPOINT="https://api.instabug.com/api/sdk/v3/symbols_files"
 
 STATUS=$(curl "${ENDPOINT}" --write-out %{http_code} --silent --output /dev/null -F os=android -F app_version="${VERSION}" -F symbols_file=@"${BITRISE_MAPPING_PATH}" -F application_token="${instabug_app_token}")
 
 if [ $STATUS -ne 200 ]; then
-  echo "Error while uploading mapping files"
+  echo "Error while uploading mapping file"
   exit 1
 fi
 
-echo "Success! Your mapping files got uploaded successfully"
+echo "Success! Your mapping file got uploaded successfully"
 
 exit 0
+
+
+
